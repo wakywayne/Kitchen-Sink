3ee47ef7e85fbe3b10af53f38ca1387171bad7fa
WayneCarl Barker <wakywayne80@gmail.com>
2021-01-27 13:35:13 -0500

    Having trouble using an array of objects and running them through a function

--- app.js
+++ app.js
@@ -6,27 +6,26 @@ function sayHello(){
     console.log("Hello World");
 }
 
-console.log(sayHello);
+
+sayHello()
 
 function checkAge(name, age){
-    if(name >= 21){
-        console.log("over21")
+    if(age >= 21){
+        console.log("over21");
     }else{
-        console.log("sorry " + name + "you are not 21 or older");
+        console.log("sorry " + name + " you are not 21 or older");
     }
 }
 
-let charles = checkAge("charles", 21);
+let charles = checkAge("Charles", 21);
 let abby = checkAge("Abby", 27);
 let james = checkAge("James", 18);
 let john = checkAge("John", 17);
 
-console.log(charles + abby + james + john);
-
 favoriteVegies = ["spinach", "broccoli", "sweetpotatoe"];
 
 let i = 0;
-while(i < 2){
+while(i < 4){
     console.log(favoriteVegies[i]);
     i++;
 }
@@ -36,6 +35,14 @@ console.log(pet.name + " " + pet.breed);
 
 person1 = {name: "p1", age: 1};
 person2 = {name: "p2", age: 2};
-person3 = {name: "p3", age: 3};
-person4 = {name: "p4", age: 4};
-person5 = {name: "p5", age: 5};
+person3 = {name: "p3", age: 31};
+person4 = {name: "p4", age: 41};
+person5 = {name: "p5", age: 51};
+
+people = [person1.name+age, person2.name+age, person3.name+age, person4.name+age, person5.name+age];
+
+let p = 0
+while(p < 5){
+    console.log(checkAge(people.p));
+    p++;
+}
